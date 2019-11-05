a2980e5d54855c7dcac4523e5dbbac973405c3a8
Zam <zam9036@163.com>
2019-11-05 12:55:50 +0800

    更新008

---
+++ Eclipse-Workspace/PRJ_BU2_JAVA_008/src/cn/campsg/practical/bubble/exception/NoClearedStarsException.java
@@ -0,0 +1,21 @@
+package cn.campsg.practical.bubble.exception;
+
+public class NoClearedStarsException extends Exception{
+
+	public NoClearedStarsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoClearedStarsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoClearedStarsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	//powered by Jingyu Zhang
+}
